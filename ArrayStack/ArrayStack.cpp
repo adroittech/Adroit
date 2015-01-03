5060409479ed69126f07c2466ef3f53aa77e9943
adroittech <nirav.faldu@gmail.com>
2015-01-03 14:25:54 +0530

    commit 2

--- ArrayStack/ArrayStack.cpp
+++ ArrayStack/ArrayStack.cpp
@@ -3,6 +3,11 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <math.h>
+
+#define max(a,b)	a>b?a:b
 
 using namespace std;
 
@@ -23,17 +28,16 @@ struct Array //: public Arrayinterface<T>
 {
 	T*		arr;
 	int		length;
-	int		actualLength;
 	
 	Array()
 	{
-		arr = new T[100];
-		actualLength = length = 100;
+		arr = new T[10];
+		length = 10;
 	};
 	Array(int size)
 	{
 		arr = new T[size];
-		actualLength = length = size;
+		length = size;
 	};
 	~Array()
 	{
@@ -42,11 +46,11 @@ struct Array //: public Arrayinterface<T>
 
 	T& operator[](int n)
 	{
-		if (n >= 0 && n < actualLength)
-			return arr[n]
+		if (n >= 0 && n < length)
+			return arr[n];
 	}
 
-	Array<T>& operator=(const Array<T>& other)
+	Array<T>& operator=(Array<T>& other)
 	{
 		if (this->arr) delete[] this->arr;
 
@@ -56,48 +60,158 @@ struct Array //: public Arrayinterface<T>
 
 		return *this;
 	}
+};
 
-	T Get(int n)
+template<typename T>
+struct ArrayStack
+{
+
+	ArrayStack() :n(0) {}
+	~ArrayStack()
 	{
-		if (n >= 0 && n < length)
+		//
+	}
+
+private:
+	Array<T>		a;
+	int				n;
+
+	T Get(int i)
+	{
+		if (i >= 0 && i < n)
 		{
-			return arr[n];
+			return a[i];
 		}
 	}
 
-	void Set(int n, T t)
+	void Set(int i, T t)
 	{
-		if (n >= 0 && n < length)
+		if (i >= 0 && i < n)
 		{
-			arr[n] = t;
+			a[i] = t;
 		}
 	}
 
-	void Add(int n, T t)
+	void Add(int i, T t)
 	{
-		if (n + 1 > length)
+		if (n + 1 > a.length)
 			Resize();
 
-		for (int i = length; i > n; --i)
-		{
-			arr[i] = arr[i - 1];
-		}
-		arr[n] = t;
+		// Traditional Way : for loop
+		// for (int j = n; j > i; --j)
+		//	a[j] = a[j - 1];
+
+		// copy_backward(a + i, a + n, a + n + 1);
+
+		a[i] = t;
+		
+		++n;
 	}
 
-	void Remove(int n)
+	void Remove(int i)
 	{
-		if ()
+		for ( int j = i; j < n ; ++j )
+		{
+			a[j] = a[j + 1];
+		}
+		--n;
+
+		if (a.length > 3*n)
+			Resize();
 	}
 
 	void Resize()
 	{
+		Array<T> b(max(2 * n, 1));
 
+		// Traditional Copy using for loop
+		for (int i = 0; i < n; ++i)
+			b[i] = a[i];
+
+		// fast copy using std::copy
+		//std::copy(a+0, a+n, b+0);
+
+		a = b;
+	}
+
+public:
+
+	int Size(){ return n; }
+
+	bool empty()
+	{
+		if (n == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	void push(T t)
+	{
+		Add(n, t);
+	}
+
+	void pop()
+	{
+		Remove(n - 1);
+	}
+
+	T top()
+	{
+		return Get(n - 1);
 	}
 };
 
+typedef struct
+{
+	int		id;
+	float	idx;
+	string	name;
+}Node;
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	/// Practice - 2
+	fstream				fs;
+	string				str;
+	ArrayStack<string>	lines;
+	
+	fs.open("../Data/FileReadTestLengthWiseSort.data", fstream::in);
+	if (fs)
+	{
+		while (fs.good())
+		{
+			getline(fs, str);
+			lines.push(str);
+		}
+	}
+
+	while (!lines.empty())
+	{
+		string str = lines.top();
+		cout << str << endl;
+		lines.pop();
+	}
+
+	/// Practice - 2 - Custon DS
+	ArrayStack<Node*>	nodes;
+
+	for (int j = 0; j < 10; ++j)
+	{
+		nodes.push(new Node{j,(float)j, "<<<----->>>" });
+	}
+
+	while (!nodes.empty())
+	{
+		Node* node = nodes.top();
+		cout << node->id << " - " << node->idx << " - " << node->name << endl;
+		nodes.pop();
+	}
+
+	cout << "Press enter to exit..." << endl;
+	getchar();
+
 	return 0;
 }
 
---
+++ std-copy/std-copy.cpp
@@ -0,0 +1,12 @@
+// std-copy.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <algorithm>
+#include <vector>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
